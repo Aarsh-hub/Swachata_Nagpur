1538764450c395cc1df670ab18bfc2e34a1308be
Aarsh-hub <24kasarea@rbunagpur.in>
2026-04-04 03:43:27 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://mkxmbmxbuqovsbugkppo.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_2CpXW63BVG9GxB00yKoMIw_SyjG_AFR';
+const SUPABASE_URL = 'https://sggpznxtvberdblwiobn.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnZ3B6bnh0dmJlcmRibHdpb2JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNDkyNTQsImV4cCI6MjA5MDgyNTI1NH0.gzKGyoVXubTmdXk-v8-BsqfkvNStGdJu7wjq0SX9W6I';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
